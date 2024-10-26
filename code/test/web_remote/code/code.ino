8705e5463d69fb27428dd613bd204bdbfd410450
azzar <azzar.mr.zs@gmail.com>
2024-10-27 03:10:48 +0700

    web

---
+++ code/test/web_remote/code/code.ino
@@ -0,0 +1,48 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "ui_index.h"  // Include the UI file
+
+// WiFi credentials
+const char* ssid = "XXXXX";
+const char* password = "drowssap";
+
+WebServer server(80);
+
+void handleRoot() {
+    server.send(200, "text/html", html);
+}
+
+void handleCommand() {
+    String cmd = server.arg("cmd");
+    
+    // Send the command through Serial
+    Serial.println(cmd);
+    
+    server.send(200, "text/plain", "Command sent: " + cmd);
+}
+
+void setup() {
+    Serial.begin(115200);
+    
+    // Connect to WiFi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    
+    Serial.println("Connected to WiFi");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    // Setup server routes
+    server.on("/", handleRoot);
+    server.on("/command", handleCommand);
+    
+    server.begin();
+    Serial.println("HTTP server started");
+}
+
+void loop() {
+    server.handleClient();
+}
