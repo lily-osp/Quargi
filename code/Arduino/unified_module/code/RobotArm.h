bed7f9ae0336b97f5d5e89f1514bf89b594eb00f
azzar budiyanto <azzar.mr.zs@gmail.com>
2024-11-17 20:49:08 +0700

    Update RobotArm.h

--- code/Arduino/unified_module/code/RobotArm.h
+++ code/Arduino/unified_module/code/RobotArm.h
@@ -63,8 +63,8 @@ class RobotArm {
     static const int STEP_ANGLE = 15;
     static const int MIN_ANGLE = 0;
     static const int MAX_ANGLE = 180;
-    static const int GRIPPER_OPEN = 0;
-    static const int GRIPPER_CLOSE = 45;
+    static const int GRIPPER_OPEN = 45;
+    static const int GRIPPER_CLOSE = 0;
     static const int HOME_BASE = 90;
     static const int HOME_SHOULDER = 90;
     static const int HOME_ELBOW = 90;
